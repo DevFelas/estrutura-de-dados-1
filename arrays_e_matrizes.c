bcf9a460bd6026a5cd4b201230fa340ae1b3d8cc
F3lip8 <128184907+DevFelas@users.noreply.github.com>
2025-10-14 16:03:13 -0300

    Update arrays_e_matrizes.c

--- arrays_e_matrizes.c
+++ arrays_e_matrizes.c
@@ -62,12 +62,52 @@ int main() {
 
 // Ler um conjunto de números inteiros, armazenando-o em vetor e calcular o quadrado das componentes deste vetor, armazenando o resultado em outro vetor. Os conjuntos têm 10 elementos cada. Imprimir todos os conjuntos.
 
-int main() {
+/* int main() {
     int valores[10], quadrados[10];
-    
-}
+
+    for (int i = 0; i < 10; i++) {
+        printf("%d Valor: ", i + 1);
+        scanf("%d", &valores[i]);
+
+        quadrados[i] = valores[i] * valores[i];
+    }
+
+    printf("%-8s- %8s\n", "valores", "quadrados");
+
+    for (int i = 0; i < 10; i++) {
+        printf("%-8d- %8d\n", valores[i], quadrados[i]);
+    }
+ 
+    return 0;
+} 
+*/ 
 
 // Faça um programa que lê três palavras do teclado e imprime as três palavras na ordem inversa.
+
+/*
+#include <string.h>
+
+int main() {
+    char palavra[50], inverso[50];
+
+    for (int i = 0; i < 3; i++) {
+        printf("%d palavra: ", i+1);
+        scanf("%s", palavra);
+
+        memset(inverso, 0, sizeof(inverso)); 
+
+        int tamanho = strlen(palavra);
+        for (int j = 0; j < tamanho; j++) {
+            inverso[j] = palavra[tamanho - j - 1];
+        }
+        inverso[tamanho] = '\n';
+
+        printf("Invertida: %s", inverso);
+    }
+    
+    return 0;
+}
+*/
 
 // Faça um programa que lê duas palavras do teclado e diz se elas são iguais ou diferentes. O programa deve dizer ainda se alguma das palavras digitadas é igual a “IFPI”.
 
